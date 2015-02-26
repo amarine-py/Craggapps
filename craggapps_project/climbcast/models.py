ba9234143674ad8544e327820392e2d5f2b45454
amarine-py <amarine@gmail.com>
2015-02-26 00:31:11 -0500

    Revert "Third commit"
    This reverts commit 51fd30615d4616b781c06414f0adae8e03d824f4.

--- craggapps_project/climbcast/models.py
+++ craggapps_project/climbcast/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class User(models.User):
-    
-    def __unicode__(self):
-        return self.username
-    
--- craggapps_project/climbcast/views.py
+++ craggapps_project/climbcast/views.py
@@ -4,15 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "Log-in Here! ... NOT!"}
-
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-
-    return render(request, 'climbcast/index.html', context_dict)
+    return HttpResponse("Test setup for CraggApps\ClimbCast <br/><a href='/climbcast/about/'>About ClimbCast</a>")
 
 def about(request):
     return HttpResponse("This is the default about view for CraggApps\ClimbCast")
