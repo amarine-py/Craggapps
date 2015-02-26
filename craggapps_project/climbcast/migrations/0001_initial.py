bfdc6ad0316a895677a0b3693871d2f9dc622c11
amarine-py <amarine@gmail.com>
2015-02-26 00:49:30 -0500

    Fourth commit
    Successfully changed User model...we should be good to go.

--- craggapps_project/climbcast/migrations/0001_initial.py
+++ craggapps_project/climbcast/migrations/0001_initial.py
@@ -2,26 +2,26 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('auth', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='User',
             fields=[
-                ('user_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('username', models.CharField(unique=True, max_length=30)),
+                ('first_name', models.CharField(max_length=50)),
+                ('last_name', models.CharField(max_length=50)),
+                ('user_email', models.EmailField(unique=True, max_length=255)),
+                ('user_password', models.CharField(max_length=50)),
             ],
             options={
-                'abstract': False,
-                'verbose_name': 'user',
-                'verbose_name_plural': 'users',
             },
-            bases=('auth.user',),
+            bases=(models.Model,),
         ),
     ]
