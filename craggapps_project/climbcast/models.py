f7a69e5020f7324816dbea6c3b6abe871800bd75
amarine-py <amarine@gmail.com>
2015-02-26 00:31:00 -0500

    Revert "Bad commit"
    This reverts commit 733237df9010bba11b831aae2c5e2aa1438723c0.

--- craggapps_project/climbcast/models.py
+++ craggapps_project/climbcast/models.py
@@ -2,13 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
-    username = models.CharField(max_length=30, unique=True)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    user_email = models.EmailField(max_length=255, unique=True)
-    user_password = models.CharField(max_length=50)
-
+class User(models.User):
+    
     def __unicode__(self):
         return self.username
     
