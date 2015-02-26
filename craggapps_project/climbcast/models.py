2cf7a8660d4f3d09871c8a3fe08cd6fc6871c24a
amarine-py <amarine@gmail.com>
2015-02-26 00:32:34 -0500

    Revert "Revert "Bad commit""
    This reverts commit f7a69e5020f7324816dbea6c3b6abe871800bd75.
    
    Conflicts:
    	craggapps_project/climbcast/models.py
    	craggapps_project/climbcast/models.pyc

--- craggapps_project/climbcast/models.py
+++ craggapps_project/climbcast/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+<<<<<<< HEAD
+=======
+
+class User(models.Model):
+    username = models.CharField(max_length=30, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    user_email = models.EmailField(max_length=255, unique=True)
+    user_password = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.username
+    
+>>>>>>> parent of f7a69e5... Revert "Bad commit"
