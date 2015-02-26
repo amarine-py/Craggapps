733237df9010bba11b831aae2c5e2aa1438723c0
amarine-py <amarine@gmail.com>
2015-02-26 00:30:54 -0500

    Bad commit
    Messed up the User model in my first migration.

--- craggapps_project/climbcast/models.py
+++ craggapps_project/climbcast/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.User):
-    
+class User(models.Model):
+    username = models.CharField(max_length=30, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    user_email = models.EmailField(max_length=255, unique=True)
+    user_password = models.CharField(max_length=50)
+
     def __unicode__(self):
         return self.username
     
