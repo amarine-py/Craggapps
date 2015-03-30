71334a6f6aa82172cb99c857c7f39c6cf71fb2d0
amarine-py <amarine@gmail.com>
2015-03-30 19:59:00 -0400

    28 - Updated models.py get_weather property
    Updated the get_weather property in the CraggAreas model to do error
    reporting

--- craggapps_project/climbcast/models.py
+++ craggapps_project/climbcast/models.py
@@ -62,98 +62,127 @@ class CraggArea(models.Model):
     def get_weather(self):
         
         # This method contains all of the variables and setups for actual weather data.
-        # Store appropriate API calls first (and parse JSON data for wunder).
-        weather_yahoo = pywapi.get_weather_from_yahoo(self.area_zip)
-        weather_com = pywapi.get_weather_from_weather_com(self.area_zip)
-        weather_wunder = urllib2.urlopen('http://api.wunderground.com/api/060cb0792aec8a1c/forecast/q/' + self.area_zip +'.json')
-        wunder_string = weather_wunder.read()
-        wunder_parsed = json.loads(wunder_string)
-        # Separate API call for wunder current weather data.
-        wunder_current = urllib2.urlopen('http://api.wunderground.com/api/060cb0792aec8a1c/conditions/q/' + self.area_zip +'.json')
-        wunder_current_string = wunder_current.read()
-        wunder_current_parsed = json.loads(wunder_current_string)
-
-        # Store current conditions for all 3 sites.
-        temp_f_yahoo = (int(weather_yahoo['condition']['temp']) * 9/5) + 32
-        temp_f_com = (int(weather_com['current_conditions']['temperature']) * 9/5) + 32
-        temp_f_wunder = int(wunder_current_parsed['current_observation']['temp_f'])        
-
-        # Create empty lists for Weater.com forecast data.
-        forecast_high_c_com = []
-        forecast_dayofweek_com = []
-        day_temp_com = []
-
-        # Populate forecast temps in celsius and convert fo fahrenheit (Weather.com)
-        for x in range(1,4):
-            forecast_high_c_com.append((int(weather_com['forecasts'][x]['high']) * 9/5) + 32)
-
-        # Populate days of week
-        for x in range(1,4):
-            forecast_dayofweek_com.append(weather_com['forecasts'][x]['day_of_week'])
-
-        # Zip the lists together for display
-        day_temp_com = zip(forecast_dayofweek_com, forecast_high_c_com)
-
-        # Create empty lists for Weather Underground forecast data
-        forecast_high_f_wunder = []
-        forecast_dayofweek_wunder = []
-        day_temp_wunder = []
-
-        # Populate forecast temps in farhenheit (Weather Underground)
-        for x in range(1,4):
-            forecast_high_f_wunder.append(wunder_parsed['forecast']['simpleforecast']['forecastday'][x]['high']['fahrenheit'])
-
-        # Populate days of week
-        for x in range(1,4):
-            forecast_dayofweek_wunder.append(wunder_parsed['forecast']['simpleforecast']['forecastday'][x]['date']['weekday'])
-                                          
-        # Zip the lists together for display
-        day_temp_wunder = zip(forecast_dayofweek_wunder, forecast_high_f_wunder)
-
-        # Create empty lists for Yahoo forecast data
-        forecast_high_c_yahoo = []
-        forecast_dayofweek_yahoo = []
-        day_temp_yahoo = []
-
-        # Populate forecast temps in fahrenheit (Yahoo)
-        for x in range(1,4):
-            forecast_high_c_yahoo.append((int(weather_yahoo['forecasts'][x]['high']) * 9/5) + 32)
-
-        # Populate days of week
-        for x in range(1,4):
-            forecast_dayofweek_yahoo.append(weather_yahoo['forecasts'][x]['day'])
-
-        # Convert to full name of day of week
-        for day in forecast_dayofweek_yahoo:
-            if x == 'Thu':
-                x = 'Thursday'
-            elif x == 'Fri':
-                x = 'Friday'
-            elif x == 'Sat':
-                x = 'Saturday'
-            elif x == 'Sun':
-                x = 'Sunday'
-            elif x == 'Mon':
-                x = 'Monday'
-            elif x == 'Tue':
-                x = 'Tuesday'
-            else:
-                x = 'Wednesday'
-
-        # Zip the lists together for display
-        day_temp_yahoo = zip(forecast_dayofweek_yahoo, forecast_high_c_yahoo)
-
-        # Initialize and store appropriate data in context_dict{}.
+        #Create a context dictionary that we can pass to template rendering engine
         context_dict = {}
+
+        # Try to get weather for Yahoo
+        try:
+            weather_yahoo = pywapi.get_weather_from_yahoo(self.area_zip)
+            temp_f_yahoo = (int(weather_yahoo['condition']['temp']) * 9/5) + 32
+
+            # Create empty lists for Yahoo forecast data
+            forecast_high_c_yahoo = []
+            forecast_dayofweek_yahoo = []
+            day_temp_yahoo = []
+
+            # Populate forecast temps in fahrenheit (Yahoo)
+            for x in range(1,4):
+                forecast_high_c_yahoo.append((int(weather_yahoo['forecasts'][x]['high']) * 9/5) + 32)
+
+            # Populate days of week
+            for x in range(1,4):
+                forecast_dayofweek_yahoo.append(weather_yahoo['forecasts'][x]['day'])
+
+            # Convert to full name of day of week
+            for day in forecast_dayofweek_yahoo:
+                if x == 'Thu':
+                    x = 'Thursday'
+                elif x == 'Fri':
+                    x = 'Friday'
+                elif x == 'Sat':
+                    x = 'Saturday'
+                elif x == 'Sun':
+                    x = 'Sunday'
+                elif x == 'Mon':
+                    x = 'Monday'
+                elif x == 'Tue':
+                    x = 'Tuesday'
+                else:
+                    x = 'Wednesday'
+
+            # Zip the lists together for display
+            day_temp_yahoo = zip(forecast_dayofweek_yahoo, forecast_high_c_yahoo)
+
+            # Put Yahoo variables into context
+            context_dict['yahoo_temp_f'] = temp_f_yahoo
+            context_dict['day_temp_yahoo'] = day_temp_yahoo
+
+        except KeyError:
+            context_dict['yahoo_temp_f'] = "No data available from Yahoo."
+            context_dict['day_temp_yahoo'] = "No data available from Yahoo."
+
+
+        # Try to get data from Weather.com
+        try: 
+            weather_com = pywapi.get_weather_from_weather_com(self.area_zip)
+            temp_f_com = (int(weather_com['current_conditions']['temperature']) * 9/5) + 32
+
+            # Create empty lists for Weather.com forecast data
+            forecast_high_c_com = []
+            forecast_dayofweek_com = []
+            day_temp_com = []
+
+            # Populate forecast temps in celsius and convert fo fahrenheit (Weather.com)
+            for x in range(1,4):
+                forecast_high_c_com.append((int(weather_com['forecasts'][x]['high']) * 9/5) + 32)
+
+            # Populate days of week
+            for x in range(1,4):
+                forecast_dayofweek_com.append(weather_com['forecasts'][x]['day_of_week'])
+
+            # Zip the lists together for display
+            day_temp_com = zip(forecast_dayofweek_com, forecast_high_c_com)
+
+            # Put Weather.com variables into context
+            context_dict['com_temp_f'] = temp_f_com
+            context_dict['day_temp_com'] = day_temp_com
+
+        except KeyError:
+            context_dict['com_temp_f'] = "No data available currently from Weather.com."
+            context_dict['day_temp_com'] = "No data available currently from Weather.com."
+
+            
+        # Try to get data from Weather Underground
+        try:
+            weather_wunder = urllib2.urlopen('http://api.wunderground.com/api/060cb0792aec8a1c/forecast/q/' + self.area_zip +'.json')
+            wunder_string = weather_wunder.read()
+            wunder_parsed = json.loads(wunder_string)
+            wunder_current = urllib2.urlopen('http://api.wunderground.com/api/060cb0792aec8a1c/conditions/q/' + self.area_zip +'.json')
+            wunder_current_string = wunder_current.read()
+            wunder_current_parsed = json.loads(wunder_current_string)
+            temp_f_wunder = int(wunder_current_parsed['current_observation']['temp_f'])
+
+            # Create empty lists for Weather Underground forecast data
+            forecast_high_f_wunder = []
+            forecast_dayofweek_wunder = []
+            day_temp_wunder = []
+
+            # Populate forecast temps in farhenheit (Weather Underground)
+            for x in range(1,4):
+                forecast_high_f_wunder.append(wunder_parsed['forecast']['simpleforecast']['forecastday'][x]['high']['fahrenheit'])
+
+            # Populate days of week
+            for x in range(1,4):
+                forecast_dayofweek_wunder.append(wunder_parsed['forecast']['simpleforecast']['forecastday'][x]['date']['weekday'])
+                                              
+            # Zip the lists together for display
+            day_temp_wunder = zip(forecast_dayofweek_wunder, forecast_high_f_wunder)
+
+            # Add variables for Weather Underground into context
+            context_dict['wunder_temp_f'] = temp_f_wunder
+            context_dict['day_temp_wunder'] = day_temp_wunder
+
+
+        except KeyError:
+            context_dict['wunder_temp_f'] = "No current data available for Weather Underground."
+            context_dict['day_temp_wunder'] = "No current data available for Weather Underground."
+            
+
+        # Add other variables into the context
         context_dict['area_name'] = self.area_name
         context_dict['area_city'] = self.area_city
         context_dict['area_state'] = self.area_state
-        context_dict['yahoo_temp_f'] = temp_f_yahoo
-        context_dict['com_temp_f'] = temp_f_com
-        context_dict['wunder_temp_f'] = temp_f_wunder
-        context_dict['day_temp_com'] = day_temp_com
-        context_dict['day_temp_yahoo'] = day_temp_yahoo
-        context_dict['day_temp_wunder'] = day_temp_wunder
+
 
         return context_dict
 
